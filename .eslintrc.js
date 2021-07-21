5ff41270adad96c4c12bdba10fd185fb6634d45c
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2021-07-21 17:24:10 +0300

    webpack config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        babelOptions: {
+            configFile: './babel.config.json',
+        }
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    extends: ['eslint:recommended', 'google'],
+    rules: {
+        'semi': 'off',
+        'comma-dangle': 'off',
+        'require-jsdoc': 'off',
+        'indent': "off",
+        'quotes': "off",
+        "linebreak-style": "off",
+        "eol-last": "off"
+    }
+}
